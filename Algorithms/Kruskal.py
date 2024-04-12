fcface6416f9db5da3418f79dd5afdf3142706d8
Sunga Hwang <sungahwang@yonsei.ac.kr>
2024-04-12 16:05:31 +0900

    Add kruskal MST code

--- Algorithms/Kruskal.py
+++ Algorithms/Kruskal.py
@@ -1,72 +1,35 @@
-import numpy as np
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
 
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [0] * size
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
 
-    def find(self, node):
-        if self.parent[node] != node:
-            self.parent[node] = self.find(self.parent[node])
-        return self.parent[node]
+    def union(self, u, v):
+        root1 = self.find(u)
+        root2 = self.find(v)
 
-    def union(self, node1, node2):
-        root1 = self.find(node1)
-        root2 = self.find(node2)
         if root1 != root2:
             if self.rank[root1] > self.rank[root2]:
                 self.parent[root2] = root1
-            elif self.rank[root1] < self.rank[root2]:
-                self.parent[root1] = root2
             else:
-                self.parent[root2] = root1
-                self.rank[root1] += 1
-            return True
-        return False
+                self.parent[root1] = root2
+                if self.rank[root1] == self.rank[root2]:
+                    self.rank[root2] += 1
 
-def kruskal(matrix):
-    edges = []
-    n = len(matrix)
-    
-    for i in range(n):
-        for j in range(i+1, n):
-            edges.append((matrix[i][j], i, j))
-    
-    edges.sort()
-    
-    uf = UnionFind(n)
-    mst = []
-    mst_weight = 0
-    
-    for weight, u, v in edges:
-        if uf.union(u, v):
-            mst.append((u, v, weight))
-            mst_weight += weight
-    
-    return mst, mst_weight
 
-matrix = [
-    [0, 0.13883805, 0.2665918, 0.31708145, -0.6050197],
-    [0.14868219, 0, -0.46095154, 0.00719108, 0.30362144],
-    [-0.29883927, 0.30510303, 0, 0.15816714, -0.19512102],
-    [0.17021039, -0.37263504, -0.04782006, 0, -1.0081314],
-    [0.42745575, 0.10338372, 0.35008624, 0.21098094, 0]
-]
+def kruskal(edges, num_nodes):
+    edges.sort() 
+    min_spanning_tree = []
+    disjoint_set = DisjointSet(num_nodes)
 
-mst, total_weight = kruskal(matrix)
-print("Minimum Spanning Tree:", mst)
-print("Total Weight:", total_weight)
+    for edge in edges:
+        weight, u, v = edge
+        if disjoint_set.find(u) != disjoint_set.find(v):
+            disjoint_set.union(u, v)
+            min_spanning_tree.append(edge)
 
-def compress_matrix(matrix, mst):
-    n = len(matrix)
-    compressed_matrix = np.zeros_like(matrix)
-    
-    for u, v, weight in mst:
-        compressed_matrix[u][v] = weight
-        compressed_matrix[v][u] = weight
-
-    return compressed_matrix
-
-compressed_matrix = compress_matrix(matrix, mst)
-print("\nCompressed Weight Matrix (kruskal):")
-print(compressed_matrix)
+    return min_spanning_tree
---
+++ Algorithms/MST.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from file_reader import read_data_from_file
+from Kruskal import kruskal
+
+def get_bottom_nodes(min_spanning_tree):
+    bottom_nodes = set()
+    all_nodes = set()
+    for edge in min_spanning_tree:
+        _, start, end = edge
+        all_nodes.add(start)
+        all_nodes.add(end)
+        if start not in [x[1] for x in min_spanning_tree]:
+            bottom_nodes.add(start)
+        elif end not in [x[1] for x in min_spanning_tree]:
+            bottom_nodes.add(end)
+    return bottom_nodes, all_nodes
+
+def remove_bottom_nodes(bottom_nodes, weights):
+    for node in bottom_nodes:
+        weights[int(node)] = 0
+    return weights
+
+def process_data_for_filters(num_filters):
+    all_bottom_nodes_per_filter = {}
+
+    for i in range(num_filters):
+        filter_name = str(i)
+        filter_path = f"relations/relations_conv1_{i}.txt"
+
+        with open(filter_path, 'r') as file:
+            matrix = []
+            for line in file:
+                line = line.strip().replace('[', '').replace(']', '').replace("'", '')
+                parts = line.strip().split(',')
+                weight = float(parts[0].strip())
+                start = int(parts[1].strip())
+                end = int(parts[2].strip())
+                matrix.append([weight, start, end])
+
+        edges = [[weight, start, end] for weight, start, end in matrix]
+
+        min_spanning_tree = kruskal(edges, num_nodes=9)
+        bottom_nodes, _ = get_bottom_nodes(min_spanning_tree)
+
+        all_bottom_nodes_per_filter[filter_name] = bottom_nodes
+
+    return all_bottom_nodes_per_filter
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc = nn.Linear(16 * 13 * 13, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+model = CNNModel()
+weights = model.state_dict()['conv1.weight'][0].cpu().detach().numpy()
+
+if __name__ == "__main__":
+    num_filters = 16 
+
+    all_bottom_nodes_per_filter = process_data_for_filters(num_filters)
+    all_weights_per_filter = {}
+
+    for filter_name, bottom_nodes in all_bottom_nodes_per_filter.items():
+        print(f"Filter {filter_name}:")
+        print("Bottom Nodes:")
+        for node in bottom_nodes:
+            print(node)
+        model_path = 'model_weights_70.pth'
+        model_weight = torch.load(model_path)['conv1.weight'][int(filter_name)].tolist()
+        model_weight = model_weight[0]
+        model_weights = model_weight[0] + model_weight[1] + model_weight[2]
+
+        weights = remove_bottom_nodes(bottom_nodes, model_weights)
+        print("\n맨 아래에 있는 모든 노드 가중치 제거 결과:")
+        print(weights)
+        conv1_weights_tensor = torch.Tensor(weights).view(1,3,3)
+        print(conv1_weights_tensor)
+        all_weights_per_filter[filter_name] = weights
+        print(all_weights_per_filter)
+
+    """
+    # 모델 저장
+    import os
+    os.makedirs('new_model', exist_ok = True)
+    torch.save(model.state_dict(), 'new_model/kruskal_weights.pth')
+    """
