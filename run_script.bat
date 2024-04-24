f67396fac605bf8851a79ff26d0bae1f17a35951
Sunga Hwang <sungahwang@yonsei.ac.kr>
2024-04-24 17:38:09 +0900

    Update batch file

--- run_script.bat
+++ run_script.bat
@@ -2,8 +2,13 @@
 
 call C:\Users\sunga\anaconda3\Scripts\activate.bat C:\Users\sunga\anaconda3\envs\sunga2
 
-python C:\MST-Compression\ResNet18\main.py
-python C:\MST-Compression\ResNet18\main_layer.py
-python C:\MST-Compression\ResNet50\main_layer.py
+python C:\MST-Compression\ResNet18\main_layer.py --dataset CIFAR10 --algorithm kruskal --pruning_method prune_model_weights --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 100
+python C:\MST-Compression\ResNet18\main_layer.py --dataset CIFAR10 --algorithm kruskal --pruning_method prune_model_filters_by_importance --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 100
+python C:\MST-Compression\ResNet18\main_layer.py --dataset CIFAR10 --algorithm prim --pruning_method prune_model_weights --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 100
+python C:\MST-Compression\ResNet18\main_layer.py --dataset CIFAR10 --algorithm prim --pruning_method prune_model_filters_by_importance --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 100
+python C:\MST-Compression\ResNet18\main_layer.py --dataset MNIST --algorithm kruskal --pruning_method prune_model_weights --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 20
+python C:\MST-Compression\ResNet18\main_layer.py --dataset MNIST --algorithm kruskal --pruning_method prune_model_filters_by_importance --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 20 
+python C:\MST-Compression\ResNet18\main_layer.py --dataset MNIST --algorithm prim --pruning_method prune_model_weights --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 20
+python C:\MST-Compression\ResNet18\main_layer.py --dataset MNIST --algorithm prim --pruning_method prune_model_filters_by_importance --target_layers layer1.0.conv1 layer2.0.conv1 layer3.0.conv1 layer4.0.conv1 --epochs 20
 
-call deactivate
+call conda deactivate
