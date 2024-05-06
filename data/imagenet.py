108e019cbe7c502f5600d628e19a433a8681c67a
Sunga Hwang <sungahwang@yonsei.ac.kr>
2024-05-07 00:39:08 +0900

    Edit imagenet download code

--- data/imagenet.py
+++ data/imagenet.py
@@ -1,9 +1,10 @@
 import os
+import uuid
 from PIL import Image
 from datasets import load_dataset
 
 # 데이터셋 다운로드
-dataset_splits = load_dataset('imagenet-1k', split=['validation', 'test'])
+dataset_splits = load_dataset('imagenet-1k', split=['validation'])
 
 # 저장할 디렉토리 생성
 validation_dir = 'data/Imagenet/validation'
@@ -14,6 +15,7 @@ os.makedirs(test_dir, exist_ok=True)
 # 클래스 목록 추출 (assumes class names are stored in a dictionary or list accessible in this way)
 class_names = dataset_splits[0].features['label'].names  # Adjust this if needed based on actual dataset structure
 
+
 # validation 데이터 저장
 for example in dataset_splits[0]:
     image = example['image']
@@ -21,15 +23,18 @@ for example in dataset_splits[0]:
     class_name = class_names[label]  # Get the class name directly using the label as index
     class_dir = os.path.join(validation_dir, class_name)
     os.makedirs(class_dir, exist_ok=True)
-    image_path = os.path.join(class_dir, f'{label}.jpeg')
+    unique_id = str(uuid.uuid4())  # Generate a unique identifier
+    image_path = os.path.join(class_dir, f'{unique_id}.jpeg')
     image.save(image_path)
 
+
 # test 데이터 저장
-for example in dataset_splits[1]:
+for example in dataset_splits[0]:
     image = example['image']
     label = example['label']
     class_name = class_names[label]  # Same as above
     class_dir = os.path.join(test_dir, class_name)
     os.makedirs(class_dir, exist_ok=True)
-    image_path = os.path.join(class_dir, f'{label}.jpeg')
+    unique_id = str(uuid.uuid4())  # Generate a unique identifier
+    image_path = os.path.join(class_dir, f'{unique_id}.jpeg')
     image.save(image_path)
